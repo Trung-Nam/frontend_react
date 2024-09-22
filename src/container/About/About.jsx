a67da049711c4997aa06a47f0492d5f5e7de8a46
Trung Nam <trungnam.work@gmail.com>
2024-09-23 00:44:45 +0700

    feat(ui,HOC):Higher-Order Components that share similar - Social media and dot navigation

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,53 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import { motion } from 'framer-motion'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
-import { images } from '../../constants'
-import './About.scss'
+import { AppWrap } from '../../wrapper';
+import './About.scss';
 import { urlFor, client } from '../../client';
 
-// const abouts = [
-//     {
-//         title: 'Web Development',
-//         description: 'I am a good web developer.',
-//         imgUrl: images.about01
-//     },
-//     {
-//         title: 'Web Design',
-//         description: 'I am know what is good web design.',
-//         imgUrl: images.about02
-//     },
-//     {
-//         title: 'UI/UX',
-//         description: 'I am know what is good UI/UX.',
-//         imgUrl: images.about03
-//     },
-//     {
-//         title: 'Web Animations',
-//         description: 'I am a good web developer.',
-//         imgUrl: images.about04
-//     },
-// ]
 const About = () => {
     const [abouts, setAbouts] = useState([]);
 
     useEffect(() => {
         const query = '*[_type == "abouts"]';
-        client.fetch(query)
-            .then((data) => {
-                setAbouts(data);
-            })
-    }, [])
 
+        client.fetch(query).then((data) => {
+            setAbouts(data);
+        });
+    }, []);
 
     return (
         <>
-            <h2 className="head-text">
-                I Know That
-                <span> Good Design</span>
-                <br />
-                means
-                <span> Good Business</span>
-            </h2>
+            <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
 
             <div className="app__profiles">
                 {abouts.map((about, index) => (
@@ -55,19 +26,17 @@ const About = () => {
                         whileInView={{ opacity: 1 }}
                         whileHover={{ scale: 1.1 }}
                         transition={{ duration: 0.5, type: 'tween' }}
-                        className='app__profile-item'
+                        className="app__profile-item"
                         key={about.title + index}
                     >
                         <img src={urlFor(about.imgUrl)} alt={about.title} />
-                        <h2 className="bold-text" style={{ marginTop: 20 }}>
-                            {about.title}
-                        </h2>
+                        <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
                         <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
                     </motion.div>
                 ))}
             </div>
         </>
-    )
-}
+    );
+};
 
-export default About
+export default AppWrap(About, 'about');
