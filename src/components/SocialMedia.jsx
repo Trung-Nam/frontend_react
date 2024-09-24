e05588233085ea702b4dce9674b29c2ad062b0ae
Trung Nam <trungnam.work@gmail.com>
2024-09-24 10:52:09 +0700

    update(ui): fix responsive and change information

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
     <div className="app__social">
         <div>
-            <BsTwitter />
-        </div>
-        <div>
             <FaFacebookF />
         </div>
         <div>
-            <BsInstagram />
+            <BsLinkedin />
+        </div>
+        <div>
+            <BsGithub />
         </div>
     </div>
 );
